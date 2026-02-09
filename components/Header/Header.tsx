b50b10cef8b8c1c687422e1b25375333c2318895
Sapper <saniamykhailenko@gmail.com>
2026-02-09 13:58:04 +0200

    start work header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,25 @@
+import Link from "next/link";
 import css from "./Header.module.css";
 
 export default async function Header() {
   return (
     <header className={css.header}>
-      <div className="container">
-        <h2>Header</h2>
+      <div className={`container ${css.headerContainer}`}>
+        <Link href="/" className={css.logo}>
+          <svg width={28} height={28}>
+            <use href="/icons.svg#ukraine"></use>
+          </svg>
+          <span>LearnLingo</span>
+        </Link>
+
+        <nav className={css.navigation}>
+          <Link href="/" className={css.navLink}>
+            Home
+          </Link>
+          <Link href="/teachers" className={css.navLink}>
+            Teachers
+          </Link>
+        </nav>
       </div>
     </header>
   );
