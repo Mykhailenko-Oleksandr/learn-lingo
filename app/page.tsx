9e15828ef3fd29af374372fb554abbd65c53e82d
Sapper <saniamykhailenko@gmail.com>
2026-02-10 17:57:13 +0200

    start work home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,19 @@ import css from "./Home.module.css";
 
 export default function Home() {
   return (
-    <section>
+    <section className={css.section}>
       <div className="container">
-        <h1>Home Page</h1>
+        <div className={css.contentBox}>
+          <h1 className={css.title}>
+            Unlock your potential with the best <span>language</span> tutors
+          </h1>
+          <p className={css.text}>
+            Embark on an Exciting Language Journey with Expert Language Tutors:
+            Elevate your language proficiency to new heights by connecting with
+            highly qualified and experienced tutors.
+          </p>
+        </div>
+        <div className={css.imageBox}></div>
       </div>
     </section>
   );
