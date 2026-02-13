3ba0d081ce7c1f9f46b7baabd0a2de6bed915266
Sapper <saniamykhailenko@gmail.com>
2026-02-13 14:22:25 +0200

    add: type teacher and upd next.config

--- types/teacher.ts
+++ types/teacher.ts
@@ -3,14 +3,14 @@ import { Reviews } from "./review";
 export interface Teacher {
   name: string;
   surname: string;
-  languages: ["English", "Spanish"];
+  languages: string[];
   levels: [
-    "A1 Beginner",
-    "A2 Elementary",
-    "B1 Intermediate",
-    "B2 Upper-Intermediate",
-    "C1 Advanced",
-    "C2 Proficient",
+    | "A1 Beginner"
+    | "A2 Elementary"
+    | "B1 Intermediate"
+    | "B2 Upper-Intermediate"
+    | "C1 Advanced"
+    | "C2 Proficient",
   ];
   rating: number;
   reviews: Reviews[];
